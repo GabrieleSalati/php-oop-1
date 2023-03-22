0b88d0045e3be1420dd2c0d504e6c29bdbc1454a
GabrieleSalati <gabriele.salati5@gmail.com>
2023-03-22 16:54:36 +0100

    Data print partially succesful

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
+<?php
+
+require_once __DIR__ . '/models/MovieClass.php';
+require_once __DIR__ . '/data/db.php';
+
+$movies_object = [];
+
+foreach ($movies as $movie) {
+    $movies_object[] = new Movie(
+        $movie['title'],
+        $movie['genre'],
+        $movie['year'],
+        $movie['director']
+    );
+}
+
+var_dump($movies_object)
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -9,7 +29,12 @@
 </head>
 
 <body>
-
+    <?php foreach ($movies_object as $movie) : ?>
+        <h2><?= $movie->title ?></h2>
+        <h2><?= $movie->genre ?></h2>
+        <h2><?= $movie->year ?></h2>
+        <h2><?= $movie->director ?></h2>
+    <?php endforeach ?>
 </body>
 
 </html>
