d673f4e25d4d09f61d708ec936ff682732f86a6c
GabrieleSalati <gabriele.salati5@gmail.com>
2023-03-22 16:32:39 +0100

    Data array ready

---
+++ data/db.php
@@ -0,0 +1,24 @@
+<?php
+
+$movies = [
+    [
+        'title' => 'Alien',
+        'genre' => 'Science fiction',
+        'year' => '1979',
+        'director' => 'Ridley Scott',
+    ],
+
+    [
+        'title' => 'Saving private Ryan',
+        'genre' => 'War movie',
+        'year' => '1998',
+        'director' => 'Steven Spielberg',
+    ],
+
+    [
+        'title' => 'Reservoir dogs',
+        'genre' => 'Crime',
+        'year' => '1992',
+        'director' => 'Quentin Tarantino',
+    ]
+];
